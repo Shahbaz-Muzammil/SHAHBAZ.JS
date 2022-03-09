1c05c98b3dcc5da29bb57750de6b11fdc34ea1b9
Shahbaz-Muzammil <97161768+Shahbaz-Muzammil@users.noreply.github.com>
2022-03-09 14:26:22 +0530

    shahbaz

---
+++ REVERSE.js
@@ -0,0 +1,33 @@
+// var number=3.14;
+// var x=80;
+// var y=90;
+//  calc =number*x*y;
+//  console.log(calc)
+
+
+// const car=[bmw,hyundai,tata,maruti,suzuki,porche]
+// for (i=0,i<car.length,i++){
+//     text+=cars[i]
+// }
+// var str="heelo,world"
+// var str2 = [...str].reverse().join(' ')
+// var str2 =str.split("").reverse().join("")
+// console.log(str2)
+
+// var str=["hello","world","morning"]
+// var str3=str.reverse()
+// console.log(str3);
+
+var str="full_name"
+// // var str1=str.slice(4,5);
+// // console.log(str1)
+// // console.log(str)
+// var str2=str.split("").filter(elem=>elem===_).toUpperCase
+// console.log(str2)
+var str1=str.replace("_",' ')
+var str2=str1.replace("f",'F')
+// str3=str1.charAt(0).toUpperCase()
+console.log(str2)
+
+
+
