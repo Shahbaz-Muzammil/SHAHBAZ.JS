6e5f39e6772c8191e5a31a9a54e702b599ffbf76
Shahbaz-Muzammil <97161768+Shahbaz-Muzammil@users.noreply.github.com>
2022-07-14 23:14:18 +0530

    some chnages

---
+++ YahooBaba/Array.js
@@ -0,0 +1,52 @@
+// const arr=[10,20,30,40,50]
+
+// sum=0
+// for( i=0; i<=4; i++){
+// console.log(arr[i]+sum)
+// }
+
+
+// const arr1=[10,20,30,40,50]
+
+// var sum=0
+
+// for( i=0; i<=4; i++){
+
+// console.log(arr[i])
+
+// sum=sum+arr[i]
+
+// }
+
+// console.log(sum)
+
+
+// const newarr= new Array([])
+
+// newarr[0]="shahbaz"
+// newarr[1]="Muzammil"
+// newarr[2]=25
+// newarr[3]="M.Sc"
+// console.log(newarr)
+
+
+// for(var i=0; i<=3; i++){
+// console.log(newarr[i])
+// }
+
+
+
+// How to fill the data of array by the user
+// const arr= new Array(3)
+
+
+// for(var i=0; i<=3; i++){
+// arr[i]=prompt("enter the value of array")
+// }
+ 
+
+// console.log(arr)
+
+
+
+// JBDCKJDSBCKJBs
