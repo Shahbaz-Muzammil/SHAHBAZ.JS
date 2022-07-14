9cb737646dedc30e555dcc37662f280a6c7a8114
Shahbaz-Muzammil <97161768+Shahbaz-Muzammil@users.noreply.github.com>
2022-07-14 23:49:06 +0530

    some changes

--- YahooBaba/Array.js
+++ YahooBaba/Array.js
@@ -47,7 +47,7 @@
 
 // console.log(arr)
 
-const arr= new Array(3)
+// const arr= new Array(3)
 
 
 // for(var i=0; i<=3; i++){
@@ -63,20 +63,27 @@ const arr= new Array(3)
 
 
 
-const data=[
-    ["shahbaz","Muzammil",25],
-    ["muzammil","shahbaz",24],
-    [25,"muzammil","shahbaz"]
-    ]
+// const data=[
+//     ["shahbaz","Muzammil",25],
+//     ["muzammil","shahbaz",24],
+//     [25,"muzammil","shahbaz"]
+//     ]
     
-    document.write("<table collapse border=1px>")
-    for(var i=0; i<data.length; i++){
-    document.write("<tr>")
-    for(var j=0; j<data.length; j++){
-    document.write("<td>",data[i][j],"</td>")
+//     document.write("<table collapse border=1px>")
+//     for(var i=0; i<data.length; i++){
+//     document.write("<tr>")
+//     for(var j=0; j<data.length; j++){
+//     document.write("<td>",data[i][j],"</td>")
     
-    }
-    document.write("</tr>")
-    document.write("<br>")
-    }
-    document.write("</table>")
+//     }
+//     document.write("</tr>")
+//     document.write("<br>")
+//     }
+//     document.write("</table>")
+
+
+    const array=["shahbaz","muzammil"]
+
+    array[0]="mujju"
+    delete array[1]
+    console.log(array)
